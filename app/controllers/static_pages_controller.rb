147ef0256975cbcfc4def920d66276836c77b233
Metaphysiker <s.raess@me.com>
2019-12-04 10:47:00 +0100

    new title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def welcome
   end
+
+  def test
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 root 'static_pages#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/test', to: 'static_pages#test', as: 'test'
 end
